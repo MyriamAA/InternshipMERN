05bb22a65fb5672f098c09aa2df345297ca3b4f9
lynn <lynneliasmansour@gmail.com>
2024-04-25 11:59:16 +0300

    updates

--- mernstack/src/index.js
+++ mernstack/src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; 
+
+// Components imports
+import CreateBook from "./components/CreateBook.js";
+import ShowBookList from "./components/ShowBookList.js";
+import ShowBookDetails from "./components/ShowBookDetails.js";
+import UpdateBookInfo from "./components/UpdateBookInfo.js";
+
+// Routes
+const router = createBrowserRouter([
+  { path: "/", element: <ShowBookList /> },
+  { path: "/create-book", element: <CreateBook /> },
+  { path: "/show-book/:id", element: <ShowBookDetails /> },
+  { path: "/edit-book/:id", element: <UpdateBookInfo /> },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
